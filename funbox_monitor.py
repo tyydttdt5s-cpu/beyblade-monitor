8c1361058a26befd41627896903b31649259332d
tyydttdt5s-cpu <h3m978@yahoo.com.tw>
2026-06-18 18:44:55 +0800

    Update funbox_monitor.py

--- funbox_monitor.py
+++ funbox_monitor.py
@@ -1,16 +1,18 @@
 import requests
-import re
 
 url = "https://shop.funbox.com.tw/collections/戰鬥陀螺"
 
 html = requests.get(
     url,
-    headers={"User-Agent":"Mozilla/5.0"},
+    headers={"User-Agent": "Mozilla/5.0"},
     timeout=30
 ).text
 
-models = sorted(set(
-    re.findall(r'(?:BX|UX|CX)-\d+', html)
-))
+idx = html.find("/products/")
 
-print(models)
+print("位置:", idx)
+
+if idx != -1:
+    print(html[idx-500:idx+1500])
+else:
+    print("找不到 /products/")
