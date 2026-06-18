92c7df8c9a72872b4f858739685588a8ed30cae6
tyydttdt5s-cpu <h3m978@yahoo.com.tw>
2026-06-18 15:26:19 +0800

    Update funbox_monitor.py

--- funbox_monitor.py
+++ funbox_monitor.py
@@ -1,33 +1,110 @@
-import requests
-import re
+Run python funbox_monitor.py
 
-url = "https://shop.funbox.com.tw/products/bbpr09652"
+===== 68051695 =====
+,{"conversion_id":"11514219669","conversion_event":"all","conversion_label":"igN2CP6_wYoaEJWZtPIq","id":null}]</script>
+<script src="//cdn-general.cybassets.com/frontend/shop/pageEventHandler-2f63d9e1f4725a36.js"></script>
+        <script>
+          window.c12t = {"customer_id":null,"product":{"id":68051695,"name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0,"brand":"Funbox Toys官方購物網站"}};
+        </script>
 
-html = requests.get(
-    url,
-    headers={"User-Agent": "Mozilla/5.0"},
-    timeout=30
-).text
+        <meta name="request-id" content="51779B94C5862AA889534FE4E5301E2E"/>
 
-patterns = [
-    "68051695",
-    "BBPR09652",
-    "products/",
-    "productId",
-    "api",
-    "graphql"
-]
+<link rel="canonical" href="https://shop.funbox.com.tw/products/bbpr09652"></link>
+<meta name="robots" content="all"></meta>
 
-for p in patterns:
-    print(f"\n===== {p} =====")
+          <script>
+            window.parent.postMessage({ type: 'previewShopInit', currentUrl: window.location.href }, '*');
+          </script>
 
-    idx = html.find(p)
+<script src="//cdn-general.cybassets.com/frontend/appStoreSDK/main.99d97ccccdb618b576bb.js"></script>
+<script>window.CYBERBIZ_AppScriptSettings = {"shop_add_ons":"W3sidmVuZG9yX3R5cGUiOiJHb29nbGUgQW5hbHl0aWNzIDQiLCJhcHBfaWQi\nOiJSYkpWMTVFcVZWOHJ1Umo3NVlmV3Z1RUlDRm90Q0FPZm0xSDZtZ3hyNkZB\nIiwic2V0dGluZ3MiOnsiZ3RhZ19pZCI6IlVBLTE0MjUwNDM0Ny0xIn19LHsi\ndmVuZG9yX3R5cGUiOiJDWUJFUkJJWiBHb29nbGUgQW5hbHl0aWNzIDQiLCJh\ncHBfaWQiOiJRVWo1WnFvZUlPUjlaM2JaS2trMG44N3l5a0FpOWU0NUREaDJ0\nYlFnaUJBIiwic2V0dGluZ3MiOnsiZ3RhZ19pZCI6IkctRUc0MkJFM1
 
-    if idx == -1:
-        print("找不到")
-        continue
+===== BBPR09652 =====
+Njk1LzExMTE0LXByb2R1Y3QtcGhvdG8tMjAyNjA1MjItNjctY2k4ajRrXzBiODhiMzc2OTc1ZGNkODBlNWM1LmpwZWciXSxbInAiLCJ0aHVtYiIsIjMyeDMyIl1d.jpeg?sha=8d85d424ad0fb6dd","description":"\u003cstrong style=\"color:#d60000;\"\u003e預購時間\u003cbr\u003e6/17（三）～ 6/21（日）\u003c/strong\u003e","brand":"Funbox Toys官方購物網站","sku":"BBPR09652","offers":[{"@type":"Offer","url":"https://shop.funbox.com.tw/products/bbpr09652","priceCurrency":"TWD","price":"999999.0","availability":"InStock","itemCondition":"https://schema.org/NewCondition","sku":"82774984"}],"productId":68051695}</script>
 
-    start = max(0, idx - 300)
-    end = idx + 1000
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          gtag('event', 'view_item', {"items":[{"id":82774984,"name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0}],"send_to":["cyberbiz","dynamic_remarketing"]});
+        })
+      </script>
+    
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          gtag('event', 'view_item', {"items":[{"id":"BBPR09652","name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0}],"send_to":["ecommerce","enhanced_ecommerce"]});
+        })
+      </script>
+    
 
-    print(html[start:end])
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          try {
+            var productData = [{"id":"BBPR09652","variant_id"
+
+===== products/ =====
+w.c12t = {"customer_id":null,"product":{"id":68051695,"name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0,"brand":"Funbox Toys官方購物網站"}};
+        </script>
+
+        <meta name="request-id" content="51779B94C5862AA889534FE4E5301E2E"/>
+
+<link rel="canonical" href="https://shop.funbox.com.tw/products/bbpr09652"></link>
+<meta name="robots" content="all"></meta>
+
+          <script>
+            window.parent.postMessage({ type: 'previewShopInit', currentUrl: window.location.href }, '*');
+          </script>
+
+<script src="//cdn-general.cybassets.com/frontend/appStoreSDK/main.99d97ccccdb618b576bb.js"></script>
+<script>window.CYBERBIZ_AppScriptSettings = {"shop_add_ons":"W3sidmVuZG9yX3R5cGUiOiJHb29nbGUgQW5hbHl0aWNzIDQiLCJhcHBfaWQi\nOiJSYkpWMTVFcVZWOHJ1Umo3NVlmV3Z1RUlDRm90Q0FPZm0xSDZtZ3hyNkZB\nIiwic2V0dGluZ3MiOnsiZ3RhZ19pZCI6IlVBLTE0MjUwNDM0Ny0xIn19LHsi\ndmVuZG9yX3R5cGUiOiJDWUJFUkJJWiBHb29nbGUgQW5hbHl0aWNzIDQiLCJh\ncHBfaWQiOiJRVWo1WnFvZUlPUjlaM2JaS2trMG44N3l5a0FpOWU0NUREaDJ0\nYlFnaUJBIiwic2V0dGluZ3MiOnsiZ3RhZ19pZCI6IkctRUc0MkJFM1dKUywg\nRy00QjdSSzdGSFdIIn19XQ==\n"}
+</script>
+<script src="https://cdn.cybassets.com/appmarket/api/common/attachments/entrypoint/dc3179e14cceab8a86a543ef3b5bcc4b0c1e5499ef19602eec341c7f6103bc9f.js"></script>
+<script src="https://cdn.cybassets.com/appmark
+
+===== productId =====
+6/17（三）～ 6/21（日）\u003c/strong\u003e","brand":"Funbox Toys官方購物網站","sku":"BBPR09652","offers":[{"@type":"Offer","url":"https://shop.funbox.com.tw/products/bbpr09652","priceCurrency":"TWD","price":"999999.0","availability":"InStock","itemCondition":"https://schema.org/NewCondition","sku":"82774984"}],"productId":68051695}</script>
+
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          gtag('event', 'view_item', {"items":[{"id":82774984,"name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0}],"send_to":["cyberbiz","dynamic_remarketing"]});
+        })
+      </script>
+    
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          gtag('event', 'view_item', {"items":[{"id":"BBPR09652","name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0}],"send_to":["ecommerce","enhanced_ecommerce"]});
+        })
+      </script>
+    
+
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          try {
+            var productData = [{"id":"BBPR09652","variant_id":82774984,"name":"【預購APP兌換限定】戰鬥陀螺 UX-00 空力天馬 3-70A 紅色版","price":999999.0,"category":"KB2X","list_name":""}];
+
+            if (document.referrer.indexOf('collection') !== -1 || document.referrer.indexOf('products') !== -1) {
+    
+
+===== api =====
+LTE0MjUwNDM0Ny0xIn19LHsi\ndmVuZG9yX3R5cGUiOiJDWUJFUkJJWiBHb29nbGUgQW5hbHl0aWNzIDQiLCJh\ncHBfaWQiOiJRVWo1WnFvZUlPUjlaM2JaS2trMG44N3l5a0FpOWU0NUREaDJ0\nYlFnaUJBIiwic2V0dGluZ3MiOnsiZ3RhZ19pZCI6IkctRUc0MkJFM1dKUywg\nRy00QjdSSzdGSFdIIn19XQ==\n"}
+</script>
+<script src="https://cdn.cybassets.com/appmarket/api/common/attachments/entrypoint/dc3179e14cceab8a86a543ef3b5bcc4b0c1e5499ef19602eec341c7f6103bc9f.js"></script>
+<script src="https://cdn.cybassets.com/appmarket/api/common/attachments/entrypoint/55265858d742072334e3b512dec79a7ea83983768ae62274cc79ac07f8061d95.js"></script>
+<script>window.CYBERBIZ.initializeSDK.initialize();</script>
+<script src="//cdn-general.cybassets.com/frontend/shop/setAffliateCookie-2f63d9e1f4725a36.js"></script>
+
+
+        <!-- LINE Tag Setting -->
+        <script>
+          var ltas = [{"customerType":"account","tagId":"4e7364c9-dc93-44cf-88b7-6fc29df56a8e"}];
+          var lts = ["4e7364c9-dc93-44cf-88b7-6fc29df56a8e"];
+        </script>
+        <!-- End LINE Tag Setting -->
+
+          <!-- LINE Tag Base Code -->
+          <!-- Do Not Modify -->
+          <script>
+          (function(g,d,o){
+            g._ltq=g._ltq||[];g._lt=g._lt||function(){g._ltq.push(arguments)};
+            var h=location.protocol==='https:'?'https://d.line-scdn.net':'http://d.line-cdn.n
+
+===== graphql =====
+找不到
