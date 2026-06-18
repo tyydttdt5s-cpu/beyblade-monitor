317f8948f008f04485ecb721a7828ac9d2a9b07a
tyydttdt5s-cpu <h3m978@yahoo.com.tw>
2026-06-18 13:47:09 +0800

    Update funbox_monitor.py

--- funbox_monitor.py
+++ funbox_monitor.py
@@ -4,10 +4,9 @@ url = "https://shop.funbox.com.tw/categories/takaratomy/beyblade"
 
 response = requests.get(
     url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
+    headers={"User-Agent": "Mozilla/5.0"},
+    timeout=30
 )
 
 print("狀態碼:", response.status_code)
-print(response.text[:500])
+print(response.text[:1000])
