7d6d1d3155fec0ec4bbd07e97a27bbb6cdf90534
tyydttdt5s-cpu <h3m978@yahoo.com.tw>
2026-06-26 19:20:17 +0800

    Update funbox_monitor.py

--- funbox_monitor.py
+++ funbox_monitor.py
@@ -5,17 +5,35 @@ url = "https://shop.funbox.com.tw/collections/beyblade-x"
 
 html = requests.get(
     url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    },
+    headers={"User-Agent":"Mozilla/5.0"},
     timeout=30
 ).text
 
-matches = re.findall(
-    r'https://[^"\']+',
+js = re.findall(
+    r'https://cdn\.cybassets\.com[^"\']+\.js',
     html
 )
 
-for m in matches:
-    if "api" in m.lower():
-        print(m)
+for j in js:
+    print("="*80)
+    print(j)
+
+    text = requests.get(
+        j,
+        headers={"User-Agent":"Mozilla/5.0"},
+        timeout=30
+    ).text
+
+    for word in [
+        "graphql",
+        "products",
+        "product",
+        "collection",
+        "inventory",
+        "variants",
+        "/api/",
+        "search"
+    ]:
+
+        if word in text.lower():
+            print("找到：", word)
